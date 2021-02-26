04ca095ba668c027dbe91f039f872badbaa350d7
linnclaesson <linn.denise.claesson@gmail.com>
2021-02-26 15:01:41 +0100

    I started to work with the Net Ninja chat example
    It is not finished

---
+++ linns-folder/get-started/example-net-ninja/index.js
@@ -0,0 +1,19 @@
+// require express
+const express = require('express');
+const socket = require('socket.io');
+
+// App setup
+const app = express();
+const server = app.listen(4000, function () {
+    console.log('Listening to request on port 4000');
+});
+
+// Static files, folder public to serv
+app.use(express.static('public'));
+
+// Socket setup
+const io = socket(server);
+
+io.on('connection', function (socket) {
+    console.log('made socket connection');
+});
