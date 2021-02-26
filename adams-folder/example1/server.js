fa257080ae224697dabe5789980db90675203c36
AdamBjork <78792864+AdamBjork@users.noreply.github.com>
2021-02-26 11:47:46 +0100

    Coding Train 12.3 adding socket.io

--- adams-folder/example1/server.js
+++ adams-folder/example1/server.js
@@ -7,3 +7,19 @@ let server = app.listen(3000);
 app.use(express.static('public'));
 
 console.log("My socket server is running");
+
+let socket = require('socket.io');
+
+let io = socket(server);
+
+io.sockets.on('connection', newConnection);
+
+function newConnection(socket) {
+    console.log('new connection' + socket.id);
+}
+
+
+
+
+
+
