8a507a798610573fbdbb8dec441673489acc4d7a
AdamBjork <adamldbjork@gmail.com>
2021-03-11 22:06:42 +0100

    Making two browser be able to send message between

--- adams-folder/example2/server.js
+++ adams-folder/example2/server.js
@@ -1,5 +1,14 @@
-const io = require('socket.io')(3000)
+//NOTE!!!! Very important to use "Go Live" with Live server on VS Code itself, otherwise it doesn't work!
+
+//Credit to "Kibae Kim", commented the solution to an error in the comments on the tutorial
+const io = require("socket.io")(3000, {
+    cors: {
+      origin: "*",
+    },
+  });
 
 io.on('connection', socket => {
-    socket.emit('chat-message', 'Hello World')
+    socket.on('send-chat-message', message => {
+        socket.broadcast.emit('chat-message', message)
+    })
 })
