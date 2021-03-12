a79530b9432a2ccec126c3cb875edd05fabf271b
AdamBjork <adamldbjork@gmail.com>
2021-03-12 13:18:51 +0100

    Adding comments to drawing example

--- adams-folder/Whiteboard-example/public/sketch.js
+++ adams-folder/Whiteboard-example/public/sketch.js
@@ -1,28 +1,36 @@
+//Creating variable
 let socket;
 
+//Setting up the canvas
 function setup() {
     createCanvas(600, 400);
     background(51);
 
+    //How users connects and calling function that draws
     socket = io.connect('http://localhost:3000');
     socket.on('mouse', newDrawing);
 }
 
+//Function that handles the data coming in from the others users
 function newDrawing(data){
     noStroke();
     fill(255, 0, 100);
     ellipse(data.x, data.y, 36, 36);
 }
 
+//Function that tells what happens when you click and drag the mouse (drawing)
 function mouseDragged() {
     console.log('Sending: ' + mouseX + ',' + mouseY);
 
+    //the data that gets transmitted and draws on the other users canvas
     let data = {
         x: mouseX,
         y: mouseY
     }
+    //emitting the data
     socket.emit('mouse', data);
 
+    //the actual drawing
     noStroke();
     fill(255);
     ellipse(mouseX, mouseY, 36, 36);
--- adams-folder/Whiteboard-example/server.js
+++ adams-folder/Whiteboard-example/server.js
@@ -1,19 +1,23 @@
-
+//Server set up
 let express = require('express');
-
 let app = express();
 let server = app.listen(3000);
 
+//Linking to the public folder
 app.use(express.static('public'));
 
+//Feedback that let's you know that it's running
 console.log("My socket server is running");
 
+//Require socket.io library
 let socket = require('socket.io');
 
 let io = socket(server);
 
+//Calling function
 io.sockets.on('connection', newConnection);
 
+//Function to what happens on connection
 function newConnection(socket) {
     console.log('new connection' + socket.id);
 
