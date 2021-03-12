9fc4ce1ad4884dfaa9ddd3a5a011863b63c56c17
AdamBjork <adamldbjork@gmail.com>
2021-03-12 13:40:32 +0100

    adding comments to the chat app

--- adams-folder/Chat-App-example/script.js
+++ adams-folder/Chat-App-example/script.js
@@ -3,22 +3,28 @@ const messageContainer = document.getElementById('message-container')
 const messageForm = document.getElementById('send-container')
 const messageInput = document.getElementById('message-input')
 
+//getting name
 const name = prompt('What is your name?')
 appendMessage('You joined')
+//emitting the name to the new user function
 socket.emit('new-user', name)
 
+//when sending a message it says the message and who sent it
 socket.on('chat-message', data => {
     appendMessage(`${data.name}: ${data.message}`)
 })
 
+//when a user connects it should say: "*their name* connected"
 socket.on('user-connected', name => {
     appendMessage(`${name} connected`)
 })
 
+//when a user disconnects it says who disconnects
 socket.on('user-disconnected', name => {
     appendMessage(`${name} disconnected`)
 })
 
+//what happens when submitting the message
 messageForm.addEventListener('submit', e => {
     e.preventDefault()
     const message = messageInput.value
@@ -27,6 +33,7 @@ messageForm.addEventListener('submit', e => {
     messageInput.value = ''
 })
 
+//display messages
 function appendMessage(message) {
     const messageElement = document.createElement('div')
     messageElement.innerText = message
--- adams-folder/Chat-App-example/server.js
+++ adams-folder/Chat-App-example/server.js
@@ -1,22 +1,28 @@
 //NOTE!!!! Very important to use "Go Live" with Live server on VS Code itself, otherwise it doesn't work!
 
-//Credit to "Kibae Kim", commented the solution to an error in the comments on the tutorial
+//Requiring socket.io library and setting the port to 3000
+//Credit to "Kibae Kim", commented the solution to an error in the comments of the tutorial
 const io = require("socket.io")(3000, {
     cors: {
       origin: "*",
     },
   });
 
-  const users = {}
+//Adding object that stores users
+const users = {}
 
+//Functions that happens on connection
 io.on('connection', socket => {
+    //handle new users
     socket.on('new-user', name => {
         users[socket.id] = name
         socket.broadcast.emit('user-connected', name)
     })
+    //handle chat events
     socket.on('send-chat-message', message => {
         socket.broadcast.emit('chat-message', { message: message, name: users[socket.id] })
     })
+    //handle what happens on disconnection
     socket.on('disconnect', () => {
         socket.broadcast.emit('user-disconnected', users[socket.id])
         delete users[socket.id]
